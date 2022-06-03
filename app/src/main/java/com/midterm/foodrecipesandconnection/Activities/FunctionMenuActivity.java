1d38ade0ac00113c8a0771ddb7502ff639f089f1
Lê Đặng Trường Đạt <45423909+ledangtruongdatdnvn@users.noreply.github.com>
2022-06-03 10:52:48 +0700

    addMenuOfFunctionsEvent

--- app/src/main/java/com/midterm/foodrecipesandconnection/Activities/FunctionMenuActivity.java
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/FunctionMenuActivity.java
@@ -1,16 +1,35 @@
 package com.midterm.foodrecipesandconnection.Activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.midterm.foodrecipesandconnection.R;
 
 
 public class FunctionMenuActivity extends AppCompatActivity {
+    CardView cv_recipes, cv_connection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function_menu);
+        cv_recipes = findViewById(R.id.cv_recipes);
+        cv_connection = findViewById(R.id.cv_connection);
+        cv_recipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FunctionMenuActivity.this, "Recipes Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cv_connection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FunctionMenuActivity.this, "Connection Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
