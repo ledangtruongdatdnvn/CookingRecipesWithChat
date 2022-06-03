4d7ef06340b423ed07c8ade8e90feef4c62839f4
Lê Đặng Trường Đạt <45423909+ledangtruongdatdnvn@users.noreply.github.com>
2022-06-03 10:32:40 +0700

    create menu of functions

---
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/FunctionMenuActivity.java
@@ -0,0 +1,16 @@
+package com.midterm.foodrecipesandconnection.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.midterm.foodrecipesandconnection.R;
+
+
+public class FunctionMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function_menu);
+    }
+}
