050914a258edfe73b00500191e13b9ecd4b39c51
Duy <anhduy070100@gmail.com>
2022-06-04 16:23:15 +0700

    add List User Layout

---
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/UsersActivity.java
@@ -0,0 +1,16 @@
+package com.midterm.foodrecipesandconnection.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.midterm.foodrecipesandconnection.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
