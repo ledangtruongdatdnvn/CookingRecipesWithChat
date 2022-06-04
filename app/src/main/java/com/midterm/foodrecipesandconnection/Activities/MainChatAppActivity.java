c1c49e28dd44fe1cfd6ef681f1699d3eb2eba404
Duy <anhduy070100@gmail.com>
2022-06-04 12:31:56 +0700

    Initialization Layout Mainchatapp

--- app/src/main/java/com/midterm/foodrecipesandconnection/Activities/MainChatAppActivity.java
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/MainChatAppActivity.java
@@ -2,15 +2,34 @@ package com.midterm.foodrecipesandconnection.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.midterm.foodrecipesandconnection.R;
+import com.midterm.foodrecipesandconnection.Utilities.Constants;
+import com.midterm.foodrecipesandconnection.Utilities.PreferenceManager;
+import com.midterm.foodrecipesandconnection.databinding.ActivityMainChatAppBinding;
 
 public class MainChatAppActivity extends AppCompatActivity {
+
+    private ActivityMainChatAppBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_chat_app);
+        binding = ActivityMainChatAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+    }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
     }
 }
