3367ca338e110868a4b6d9d065d8bb89a142119e
Duy <anhduy070100@gmail.com>
2022-06-05 22:37:58 +0700

    Click on User List to the chat screen

--- app/src/main/java/com/midterm/foodrecipesandconnection/Activities/ChatActivity.java
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/ChatActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.midterm.foodrecipesandconnection.Models.User;
 import com.midterm.foodrecipesandconnection.R;
+import com.midterm.foodrecipesandconnection.Utilities.Constants;
+import com.midterm.foodrecipesandconnection.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private ActivityChatBinding binding;
+    private User receiverUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+        loadReceiverDetails();
+    }
+
+    private void loadReceiverDetails() {
+        receiverUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
+        binding.textName.setText(receiverUser.name);
+    }
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
     }
 }
