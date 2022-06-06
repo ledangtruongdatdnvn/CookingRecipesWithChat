bdbe72562c190f94bfea23f158993cee8e8aa85d
Lê Đặng Trường Đạt <45423909+ledangtruongdatdnvn@users.noreply.github.com>
2022-06-06 13:18:28 +0700

    add instruction adapters

---
+++ app/src/main/java/com/midterm/foodrecipesandconnection/View/InstructionsEquipmentsAdapter.java
@@ -0,0 +1,60 @@
+package com.midterm.cookingrecipe.View;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.midterm.cookingrecipe.Models.Equipment;
+import com.midterm.cookingrecipe.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class InstructionsEquipmentsAdapter extends RecyclerView.Adapter<InstructionsEquipmentsAdapter.InstructionsEquipmentsViewHolder> {
+
+    Context context;
+    List<Equipment> list;
+
+    public InstructionsEquipmentsAdapter(Context context, List<Equipment> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public InstructionsEquipmentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new InstructionsEquipmentsViewHolder(LayoutInflater.from(context).inflate(R.layout.list_instructions_step_items, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InstructionsEquipmentsViewHolder holder, int position) {
+        holder.textView_instructions_step_item.setText(list.get(position).getName());
+        holder.textView_instructions_step_item.setSelected(true);
+        Picasso.get().load("https://spoonacular.com/cdn/equipment_100x100/" + list.get(position).getImage()).into(holder.imageView_instructions_step_item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class InstructionsEquipmentsViewHolder extends RecyclerView.ViewHolder {
+        TextView textView_instructions_step_item;
+
+        ImageView imageView_instructions_step_item;
+
+        public InstructionsEquipmentsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView_instructions_step_item = itemView.findViewById(R.id.imageView_instructions_step_item);
+            textView_instructions_step_item = itemView.findViewById(R.id.textView_instructions_step_item);
+        }
+    }
+}
+
+
