7258e7e14e68f571c0aa3a8c99db0933cbcca05d
Duy <anhduy070100@gmail.com>
2022-06-07 10:09:40 +0700

    Add Conversion Listener function And last message updated And new conversation added, the conversion will move to the top as it has a new message

--- app/src/main/java/com/midterm/foodrecipesandconnection/Activities/MainChatAppActivity.java
+++ app/src/main/java/com/midterm/foodrecipesandconnection/Activities/MainChatAppActivity.java
@@ -18,7 +18,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.midterm.foodrecipesandconnection.Adapters.RecentConversationsAdapter;
+import com.midterm.foodrecipesandconnection.Listeners.ConversionListener;
 import com.midterm.foodrecipesandconnection.Models.ChatMessage;
+import com.midterm.foodrecipesandconnection.Models.User;
 import com.midterm.foodrecipesandconnection.Utilities.Constants;
 import com.midterm.foodrecipesandconnection.Utilities.PreferenceManager;
 import com.midterm.foodrecipesandconnection.databinding.ActivityMainChatAppBinding;
@@ -28,7 +30,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainChatAppActivity extends AppCompatActivity {
+public class MainChatAppActivity extends AppCompatActivity implements ConversionListener {
 
     private ActivityMainChatAppBinding binding;
     private PreferenceManager preferenceManager;
@@ -51,7 +53,7 @@ public class MainChatAppActivity extends AppCompatActivity {
 
     private void init() {
         conversations = new ArrayList<>();
-        conversationsAdapter = new RecentConversationsAdapter(conversations);
+        conversationsAdapter = new RecentConversationsAdapter(conversations, this);
         binding.conversationsRecyclerView.setAdapter(conversationsAdapter);
         database = FirebaseFirestore.getInstance();
     }
@@ -98,15 +100,15 @@ public class MainChatAppActivity extends AppCompatActivity {
                         chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_RECEIVER_IMAGE);
                         chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_RECEIVER_NAME);
                         chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
-                    }else {
+                    } else {
                         chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_SENDER_IMAGE);
-                        chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_SENDER_IMAGE);
-                        chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                        chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_SENDER_NAME);
+                        chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
                     }
                     chatMessage.message = documentChange.getDocument().getString(Constants.KEY_LAST_MESSAGE);
                     chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
                     conversations.add(chatMessage);
-                } else if (documentChange.getType() == DocumentChange.Type.MODIFIED) {
+                }else if (documentChange.getType() == DocumentChange.Type.MODIFIED) {
                     for (int i = 0; i < conversations.size(); i++) {
                         String senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
                         String receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
@@ -156,5 +158,12 @@ public class MainChatAppActivity extends AppCompatActivity {
                     finish();
                 })
                 .addOnFailureListener(e -> showToast("Unable to sign out"));
+    }
+
+    @Override
+    public void onConversionClicked(User user) {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
     }
 }
